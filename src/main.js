c3b572e26fd4c89bd60d748f78b4251ffdac5f98
RachelMarotta <kelmarotta@icloud.com>
2019-09-12 13:13:18 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const disable = () =>{
   document.getElementById("final-year").setAttribute("disabled", "");
-}
+};
 
 document.getElementById("one-year").addEventListener("click", disable);
 
 const enable = () =>{
   document.getElementById("final-year").removeAttribute("disabled", "");
-}
+};
 
-document.getElementById("period").addEventListener("click", enable)
+document.getElementById("period").addEventListener("click", enable);
