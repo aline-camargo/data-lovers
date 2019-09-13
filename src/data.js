19780c7669a5b21f0f437a087fc9ea29165f0afc
aline-camargo <aline97camargo@gmail.com>
2019-09-13 01:04:22 -0300

    mostrando tabela, correções css

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
 window.INJURIES = INJURIES;
 
+const carAccidents = ["Total_Injured_Persons_Passenger_Car_Occupants",
+"Total_Injured_Persons_Passenger_Or_Occupant"];
+
+const carsTotalAccidents = () =>{
+  const cars = INJURIES.reduce(
+    (total, year) => total + year[carAccidents[0]] + year[carAccidents[1]],
+    0,
+    );
+  return cars;
+}
+
+const motosTotalAccidents = () =>{
+  const motos = INJURIES.reduce(
+    (total, year) => total + year.Total_Injured_Persons_Motorcyclists,
+    0,
+    );
+  return motos;
+};
+
 const filterPeriod = (initialYear, finalYear) => {
 const period = INJURIES.filter(injurie => (injurie.Year.slice(0, 4) >= initialYear && injurie.Year.slice(0, 4) <= finalYear));
 
   return period;
-}; 
+};
 
 window.filterPeriod = filterPeriod;
+window.carsTotalAccidents = carsTotalAccidents;
+window.motosTotalAccidents = motosTotalAccidents;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,41 @@
+const initialTable = () =>{
+  carsTd();
+  motosTd();
+  allTd();
+}
+
+const allTd = () =>{
+  const allAccidents = window.carsTotalAccidents() + window.motosTotalAccidents();
+  const newTd = document.createElement("td");
+  newTd.innerHTML = allAccidents;
+  document.getElementById("total-results").appendChild(newTd);
+}
+
+const carsTd = () =>{
+  const carAccidents = window.carsTotalAccidents();
+  const newTd = document.createElement("td");
+  newTd.innerHTML = carAccidents;
+  document.getElementById("total-results").appendChild(newTd);
+}
+
+const motosTd = () =>{
+  const motosAccidents = window.motosTotalAccidents();
+  const newTd = document.createElement("td");
+  newTd.innerHTML = motosAccidents;
+  document.getElementById("total-results").appendChild(newTd);
+}
+
 const disable = () =>{
   document.getElementById("final-year").setAttribute("disabled", "");
+  document.getElementById("final-year-label").style.color = "grey";
+  document.getElementById("initial-year-label").innerHTML = "Ano";
 };
-
-document.getElementById("one-year").addEventListener("click", disable);
 
 const enable = () =>{
   document.getElementById("final-year").removeAttribute("disabled", "");
+  document.getElementById("final-year-label").style.color = "white";
+  document.getElementById("initial-year-label").innerHTML = "Ano Inicial";
 };
-
-document.getElementById("period").addEventListener("click", enable);
 
 const search = () =>{
   const initialYear = Number(document.getElementById("initial-year").value);
@@ -20,4 +47,7 @@ const search = () =>{
   console.log(window.filterPeriod(initialYear, finalYear));
 };
 
+window.addEventListener("load", initialTable);
+document.getElementById("one-year").addEventListener("click", disable);
+document.getElementById("period").addEventListener("click", enable);
 document.getElementById("search").addEventListener("click", search);
