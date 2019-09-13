347d00efc717c43935c5456836e0c4b0e367997e
RachelMarotta <kelmarotta@icloud.com>
2019-09-13 13:49:38 -0300

    filtrar acidentes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,28 @@
 window.INJURIES = INJURIES;
 
-const filterPeriod = (initialYear, finalYear) => {
-const period = INJURIES.filter(injurie => (injurie.Year.slice(0, 4) >= initialYear && injurie.Year.slice(0, 4) <= finalYear));
+const filterPeriod = (initialYear, finalYear, selectTransport) => {
+  const period = INJURIES.filter(injurie => (injurie.Year.slice(0, 4) >= initialYear && injurie.Year.slice(0, 4) <= finalYear));
+  
+  const injurieTransportMoto = (period) => {
+    const injuriemoto = period.map(injurie => `${injurie.Total_Injured_Persons_Motorcyclists}`);
+    console.log(injuriemoto);
+  };
+  
+  const injurieTransportCar = (period) => {
+    const injuriecar = period.map(injurie => `${injurie.Total_Injured_Persons_Passenger_Car_Occupants + injurie.Total_Injured_Persons_Passenger_Or_Occupant}`);
+    console.log(injuriecar);
+  };
+  if (selectTransport == "car") {
+    //console.log(injurieTransportCar(period))
+    return injurieTransportCar(period);
 
-  return period;
+  } else if (selectTransport == "motorcycle") {
+    return injurieTransportMoto(period);
+
+  } else if (selectTransport == "all") {
+    return injurieTransportCar(period) + injurieTransportMoto(period);
+  }
 }; 
 
 window.filterPeriod = filterPeriod;
+
--- src/main.js
+++ src/main.js
@@ -13,11 +13,17 @@ document.getElementById("period").addEventListener("click", enable);
 const search = () =>{
   const initialYear = Number(document.getElementById("initial-year").value);
   let finalYear = Number(document.getElementById("final-year").value);
+  const selectTransport = document.getElementById("transport").value;
 
   if (finalYear === 0) {
     finalYear = initialYear;
   }
-  console.log(window.filterPeriod(initialYear, finalYear));
+  const result = window.filterPeriod(initialYear, finalYear, selectTransport); 
+
+  console.log(result);
+  //const console = result.map(console.log());
+  //return console;
 };
 
 document.getElementById("search").addEventListener("click", search);
+
