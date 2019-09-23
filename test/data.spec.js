81b04a4ec990aaafc8c97fdaf13c9a06bfea10c6
aline-camargo <aline97camargo@gmail.com>
2019-09-23 14:18:43 -0300

    testes filterPeriod rodando

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,21 +1,40 @@
+/* eslint camelcase: "off"*/
+
 const origin = require("../src/data.js");
 
-const arrData = [{Year: "1960-01-04"}, {Year: "2000-01-04"}, {Year: "2001-01-04"}, {Year: "2019-01-04"}, {Year: "2011-01-04"}];
-// const filterYears = src.window.data.filterYears;
-describe("testando filterYears", () =>{
-  it("filterYears retorna array filtrada", () => {
-    expect(origin.filterPeriod(arrData, 2000, 2011)).toEqual([{Year: "2000-01-04"}, {Year: "2001-01-04"}, {Year: "2011-01-04"}]);
+const arrDataYears = [{Year: "1960-01-04"}, {Year: "2000-01-04"}, {Year: "2001-01-04"}, {Year: "2019-01-04"}, {Year: "2011-01-04"}];
+const arrDataTransport = [{Total_Injured_Persons_Passenger_Car_Occupants: 2051609, Total_Injured_Persons_Passenger_Or_Occupant: null, Total_Injured_Persons_Motorcyclists: 57723},
+  {Total_Injured_Persons_Passenger_Car_Occupants: 1474536, Total_Injured_Persons_Passenger_Or_Occupant: 15702, Total_Injured_Persons_Motorcyclists: 88652},
+  {Total_Injured_Persons_Passenger_Car_Occupants: 1378000, Total_Injured_Persons_Passenger_Or_Occupant: 18895, Total_Injured_Persons_Motorcyclists: 88000}];
+
+describe("Testando filterPeriod", () =>{
+  test("É um objeto", () =>{
+    expect(typeof origin.filterPeriod(arrDataYears, 2000, 2001)).toEqual("object");
   });
 
-  it("exibe mensagem quando year é letra", () =>{
-    expect(origin.filterPeriod(arrData, 0, 2010)).toEqual("Inválido");
+  test("Retorna array filtrada", () => {
+    expect(origin.filterPeriod(arrDataYears, 2000, 2011)).toEqual([{Year: "2000-01-04"}, {Year: "2001-01-04"}, {Year: "2011-01-04"}]);
+  });
+
+  test("Exibe mensagem quando year é letra", () =>{
+    expect(origin.filterPeriod(arrDataYears, 0, 2010)).toEqual("Caractere Inválido");
+  });
+
+  test("Exibe mensagem quando initialYear é menor que 2000 ou finalYear é maior que 2015", () =>{
+    expect(origin.filterPeriod(arrDataYears, 1960, 2010)).toEqual("Período Inválido");
+  });
+
+  test("Período quando finalYear < initialYear", () => {
+    expect(origin.filterPeriod(arrDataYears, 2001, 2000)).toEqual("Período Inválido");
+  });
+});
+
+describe("Testando filterTransport", () =>{
+  test("É um objeto", () =>{
+    expect(typeof origin.filterTransport(arrDataTransport, "Carro")).toEqual("object");
+  });
+  test("Filtra por transporte", () =>{
+    expect(origin.filterTransport(arrDataTransport, "Carro")).toEqual([2051609, 1490238, 1396895]);
   });
 
 });
-//   it("returns `example`", () => {
-//     expect(example()).toBe("example");
-//   });
-// });
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
