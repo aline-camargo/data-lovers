ba9129288792d02236a7c9ab7406fbaea5cf4dc3
Aline C <aline.m.cmrg@gmail.com>
2021-08-06 12:52:07 -0300

    Removing semicolon

--- src/data.js
+++ src/data.js
@@ -1,49 +1,49 @@
 const validatePeriod = (data, initialYear, finalYear) => {
   if (initialYear === 0 || finalYear === 0) {
-    return "Caractere Inválido";
+    return "Caractere Inválido"
   } else if (initialYear < 2000 || finalYear > 2015 || finalYear < initialYear) {
-    return "Período Inválido";
+    return "Período Inválido"
   } else {
-    return filterPeriod(data, initialYear, finalYear);
+    return filterPeriod(data, initialYear, finalYear)
   }
-};
+}
 
 const filterPeriod = (data, initialYear, finalYear) => {
-  return data.filter(injurie => injurie.year >= initialYear && injurie.year <= finalYear);
-};
+  return data.filter(injurie => injurie.year >= initialYear && injurie.year <= finalYear)
+}
 
 const totalAccidents = (injurie, transport) => {
-  if (transport != "total") {   
-    return injurie.reduce((total, accident) => total + accident[transport.toLowerCase()], 0);
+  if (transport != "total") {
+    return injurie.reduce((total, accident) => total + accident[transport.toLowerCase()], 0)
   } else {
     return injurie.reduce((acc, cur) => ({
       carros: acc.carros + cur.carros,
       motos: acc.motos + cur.motos,
       total: acc.total + cur.total,
-    }), { carros: 0, motos: 0, total: 0 });
+    }), { carros: 0, motos: 0, total: 0 })
   }
-};
+}
 
 const average = (totalAccidents, divider) => {
   if (typeof totalAccidents === "number") {
-    return parseInt(totalAccidents / divider);
+    return parseInt(totalAccidents / divider)
   } else {
-    return totalAccidents.map(element => parseInt(element/divider));
+    return totalAccidents.map(element => parseInt(element/divider))
   }
-};
+}
 
 const orderAccidents = (tableBase, order, selectTransport, allTableOrderChoice) =>{
-  const selectOrder = selectTransport === "total" ? allTableOrderChoice : selectTransport;
+  const selectOrder = selectTransport === "total" ? allTableOrderChoice : selectTransport
 
-  const compare = (a, b) =>{if (parseInt(a[selectOrder]) < parseInt(b[selectOrder])) {return 1;} else {return -1;}};
-  const compare2 =(a, b) =>{if (parseInt(a[selectOrder]) > parseInt(b[selectOrder])) {return 1;} else {return -1;}};
+  const compare = (a, b) =>{if (parseInt(a[selectOrder]) < parseInt(b[selectOrder])) {return 1} else {return -1}}
+  const compare2 =(a, b) =>{if (parseInt(a[selectOrder]) > parseInt(b[selectOrder])) {return 1} else {return -1}}
 
   if (order === "crescent" || order === "older") {
-    return tableBase.sort(compare2);
+    return tableBase.sort(compare2)
   } else {
-    return tableBase.sort(compare);
+    return tableBase.sort(compare)
   }
-};
+}
 
 export {
   filterPeriod,
@@ -51,4 +51,4 @@ export {
   totalAccidents,
   orderAccidents,
   average,
-};
+}
--- src/main.js
+++ src/main.js
@@ -3,136 +3,136 @@ import {
   validatePeriod,
   totalAccidents,
   orderAccidents,
-  average } from "./data.js";
+  average } from "./data.js"
 
-const selectTransport = document.getElementById("transport");
+const selectTransport = document.getElementById("transport")
 
 const getInjuries = () => {
   return fetch("https://raw.githubusercontent.com/aline-camargo/SAP003-data-lovers/master/src/data/injuries/injuries.json")
     .then(response => response.json())
     .then(data => {
       const finalData = data.filter(injurie => {
-        const selectedYears = injurie.Year.slice(0, 4);
-        return selectedYears >= 2000 && selectedYears <= 2015;
-      });
+        const selectedYears = injurie.Year.slice(0, 4)
+        return selectedYears >= 2000 && selectedYears <= 2015
+      })
 
       return finalData.map(element => {
-        const carros = element.Total_Injured_Persons_Passenger_Car_Occupants + element.Total_Injured_Persons_Passenger_Or_Occupant;
-        const motos = element.Total_Injured_Persons_Motorcyclists;
-        const total = carros + motos;
-        return { carros, motos, total, year: element.Year.slice(0, 4) };
-      });  
-    });
-};
+        const carros = element.Total_Injured_Persons_Passenger_Car_Occupants + element.Total_Injured_Persons_Passenger_Or_Occupant
+        const motos = element.Total_Injured_Persons_Motorcyclists
+        const total = carros + motos
+        return { carros, motos, total, year: element.Year.slice(0, 4) }
+      })  
+    })
+}
 
 const showTotalTable = (injuries) => {
   const totalInjuries = injuries.reduce((acc, cur) => ({
     carros: acc.carros + cur.carros,
     motos: acc.motos + cur.motos,
     total: acc.total + cur.total,
-  }), { carros: 0, motos: 0, total: 0 });
+  }), { carros: 0, motos: 0, total: 0 })
   
   document.getElementById("initial-total-results").innerHTML = `
     <td>${totalInjuries.carros}</td>
     <td>${totalInjuries.motos}</td>
     <td>${totalInjuries.total}</td>
-  `;
-};
+  `
+}
 
 const disable = () =>{
-  document.getElementById("final-year").setAttribute("disabled", "");
-  document.getElementById("final-year-label").style.color = "grey";
-  document.getElementById("initial-year-label").textContent = "Ano";
-};
+  document.getElementById("final-year").setAttribute("disabled", "")
+  document.getElementById("final-year-label").style.color = "grey"
+  document.getElementById("initial-year-label").textContent = "Ano"
+}
 
 const enable = () =>{
-  document.getElementById("final-year").removeAttribute("disabled", "");
-  document.getElementById("final-year-label").style.color = "white";
-  document.getElementById("initial-year-label").textContent = "Ano Inicial";
-};
+  document.getElementById("final-year").removeAttribute("disabled", "")
+  document.getElementById("final-year-label").style.color = "white"
+  document.getElementById("initial-year-label").textContent = "Ano Inicial"
+}
 
 const checkRadio = (adicionalFilters, initialYear, finalYear) =>{
   if (document.getElementById("one-year").checked) {
-    adicionalFilters.forEach(element => element.setAttribute("hidden", ""));
-    return initialYear;
+    adicionalFilters.forEach(element => element.setAttribute("hidden", ""))
+    return initialYear
   } else if (finalYear === initialYear) {
-    document.getElementById("error-message").textContent = "Selecione \"Apenas um ano\"";
-    adicionalFilters.forEach(element => element.setAttribute("hidden", ""));
+    document.getElementById("error-message").textContent = "Selecione \"Apenas um ano\""
+    adicionalFilters.forEach(element => element.setAttribute("hidden", ""))
   } else {
-    adicionalFilters.forEach(element => element.removeAttribute("hidden", ""));
+    adicionalFilters.forEach(element => element.removeAttribute("hidden", ""))
   }
-  return finalYear;
-};
+  return finalYear
+}
 
 const cleanMain = () => {
-  document.getElementById("main-table").setAttribute("hidden", "");
-  document.getElementById("error-message").textContent = "";
-};
+  document.getElementById("main-table").setAttribute("hidden", "")
+  document.getElementById("error-message").textContent = ""
+}
 
 const search = () =>{
-  cleanMain();
+  cleanMain()
 
-  const initialYear = +document.getElementById("initial-year").value;
-  const finalYear = +document.getElementById("final-year").value;
-  const order = document.getElementById("order").value;
-  const hideElements = document.getElementsByName("hide");
-  const checkedFinalYear = checkRadio(hideElements, initialYear, finalYear);
+  const initialYear = +document.getElementById("initial-year").value
+  const finalYear = +document.getElementById("final-year").value
+  const order = document.getElementById("order").value
+  const hideElements = document.getElementsByName("hide")
+  const checkedFinalYear = checkRadio(hideElements, initialYear, finalYear)
 
-  const injuries = JSON.parse(localStorage.getItem("injuries"));
-  const period = validatePeriod(injuries, initialYear, checkedFinalYear);
+  const injuries = JSON.parse(localStorage.getItem("injuries"))
+  const period = validatePeriod(injuries, initialYear, checkedFinalYear)
 
   if (typeof period === "string") {
-    document.getElementById("table-results").setAttribute("hidden", "");
-    hideElements.forEach(element => element.setAttribute("hidden", ""));
-    document.getElementById("error-message").textContent = period;
+    document.getElementById("table-results").setAttribute("hidden", "")
+    hideElements.forEach(element => element.setAttribute("hidden", ""))
+    document.getElementById("error-message").textContent = period
   }
 
   if (!selectTransport.value) {
-    document.getElementById("table-results").setAttribute("hidden", "");
-    document.getElementById("error-message").textContent = "Escolha um transporte";
+    document.getElementById("table-results").setAttribute("hidden", "")
+    document.getElementById("error-message").textContent = "Escolha um transporte"
   }
   
-  const accidentsTotal = totalAccidents(period, selectTransport.value);
+  const accidentsTotal = totalAccidents(period, selectTransport.value)
 
-  const allTableOrderChoice = document.querySelector("input[name='hide-choice']:checked").value;
-  orderAccidents(period, order, selectTransport.value.toLowerCase(), allTableOrderChoice);
+  const allTableOrderChoice = document.querySelector("input[name='hide-choice']:checked").value
+  orderAccidents(period, order, selectTransport.value.toLowerCase(), allTableOrderChoice)
   
   if (document.getElementById("error-message").textContent != "") {
-    document.getElementById("table-results").setAttribute("hidden", "");
+    document.getElementById("table-results").setAttribute("hidden", "")
   } else if (selectTransport.value == "total") {
-    moreThanOneTable(period, accidentsTotal);
+    moreThanOneTable(period, accidentsTotal)
   } else {
-    resultTable(period, accidentsTotal, selectTransport.value);
+    resultTable(period, accidentsTotal, selectTransport.value)
   }
-};
+}
 
 const resultTable = (period, accidentsTotal, transport) =>{
-  const hideChoice = document.getElementsByName("hide-choice");
-  hideChoice.forEach(element => element.setAttribute("hidden", ""));
-  const rowsTemplate = period.map(element => `<tr><td>${element.year}</td><td>${element[transport.toLowerCase()]}</td></tr>`).join("");
-  document.getElementById("table-results").removeAttribute("hidden", "");
-  document.getElementById("t-head").innerHTML = `<th colspan="2">Acidentes de ${transport}</th>`;
+  const hideChoice = document.getElementsByName("hide-choice")
+  hideChoice.forEach(element => element.setAttribute("hidden", ""))
+  const rowsTemplate = period.map(element => `<tr><td>${element.year}</td><td>${element[transport.toLowerCase()]}</td></tr>`).join("")
+  document.getElementById("table-results").removeAttribute("hidden", "")
+  document.getElementById("t-head").innerHTML = `<th colspan="2">Acidentes de ${transport}</th>`
   document.getElementById("t-body").innerHTML = `
     <tr class="main-table-subtitle"><td>Ano</td><td>Número de Acidentes</td></tr>
     ${rowsTemplate}
     <tr><td class="total">Total</td><td id="totalDeAcidentes">${accidentsTotal}</td></tr>
-  `;
-};
+  `
+}
 
 const moreThanOneTable = (tableBase, totalAccidents) =>{
 
-  const hideChoice = document.getElementsByName("hide-choice");
+  const hideChoice = document.getElementsByName("hide-choice")
   if (document.getElementById("one-year").checked) {
-    hideChoice.forEach(element => element.setAttribute("hidden", ""));
+    hideChoice.forEach(element => element.setAttribute("hidden", ""))
   } else {
-    hideChoice.forEach(element => element.removeAttribute("hidden", ""));
-  };
+    hideChoice.forEach(element => element.removeAttribute("hidden", ""))
+  }
 
   const rowsTemplate = tableBase.map(element => `
   <tr><td>${element.year}</td><td>${element.carros}</td><td>${element.motos}</td><td>${element.total}</td></tr>
-  `).join("");
-  document.getElementById("table-results").removeAttribute("hidden", "");
-  document.getElementById("t-head").innerHTML = "<th colspan=\"4\">Total de Acidentes</th>";
+  `).join("")
+  document.getElementById("table-results").removeAttribute("hidden", "")
+  document.getElementById("t-head").innerHTML = "<th colspan=\"4\">Total de Acidentes</th>"
   document.getElementById("t-body").innerHTML = `
   <tr class="main-table-subtitle">
     <td>Ano</td><td>Carro</td>
@@ -146,40 +146,40 @@ const moreThanOneTable = (tableBase, totalAccidents) =>{
     <td class="total-accidents">${totalAccidents.motos}</td>
     <td class="total-accidents">${totalAccidents.total}</td>
   </tr>
-  `;  
-};
+  `  
+}
 
 const averageGetter = () =>{
-  const initialYear = +document.getElementById("initial-year").value;
-  const finalYear = +document.getElementById("final-year").value;
-  const divider = finalYear - initialYear + 1;
+  const initialYear = +document.getElementById("initial-year").value
+  const finalYear = +document.getElementById("final-year").value
+  const divider = finalYear - initialYear + 1
 
   if (selectTransport.value === "total") {
-    const totalDeAcidentes = Array.from(document.querySelectorAll(".total-accidents")).map(element => Number(element.textContent));
-    const resultAverage = average(totalDeAcidentes, divider);
+    const totalDeAcidentes = Array.from(document.querySelectorAll(".total-accidents")).map(element => Number(element.textContent))
+    const resultAverage = average(totalDeAcidentes, divider)
     document.getElementById("t-body").innerHTML += `<tr><td class="total">Médias</td><td>${resultAverage[0]}</td>
-      <td>${resultAverage[1]}</td><td>${resultAverage[2]}</td></tr>`;      
+      <td>${resultAverage[1]}</td><td>${resultAverage[2]}</td></tr>`      
   } else {
-    const totalDeAcidentes = +document.querySelector("#totalDeAcidentes").textContent;
-    const resultAverage = average(totalDeAcidentes, divider);
-    document.getElementById("t-body").innerHTML += `<tr><td class="total">Média</td><td>${resultAverage}</td></tr>`;
+    const totalDeAcidentes = +document.querySelector("#totalDeAcidentes").textContent
+    const resultAverage = average(totalDeAcidentes, divider)
+    document.getElementById("t-body").innerHTML += `<tr><td class="total">Média</td><td>${resultAverage}</td></tr>`
   }
-};
+}
 
 window.addEventListener("load", () => {
-  const injuries = JSON.parse(localStorage.getItem("injuries"));
+  const injuries = JSON.parse(localStorage.getItem("injuries"))
   if (injuries) {
-    showTotalTable(injuries);
+    showTotalTable(injuries)
   } else {
     getInjuries()
       .then(data => {
-        showTotalTable(data);
-        localStorage.setItem("injuries", JSON.stringify(data));
-      });
+        showTotalTable(data)
+        localStorage.setItem("injuries", JSON.stringify(data))
+      })
   }
-});
-document.getElementById("one-year").addEventListener("change", disable);
-document.getElementById("period").addEventListener("change", enable);
-document.getElementById("search").addEventListener("click", search);
-document.getElementById("average").addEventListener("click", averageGetter);
-document.getElementById("order").addEventListener("change", search);
+})
+document.getElementById("one-year").addEventListener("change", disable)
+document.getElementById("period").addEventListener("change", enable)
+document.getElementById("search").addEventListener("click", search)
+document.getElementById("average").addEventListener("click", averageGetter)
+document.getElementById("order").addEventListener("change", search)
